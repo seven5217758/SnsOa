765d0a6a79dbdd5b4600a713e33d74828510689b
dongchenglong <dcl8261425@gamil.com>
2012-07-23 20:23:02 +0800

    Revert "main project"
    This reverts commit 991032c01b8986e89ed2f6606df6b97640689398.

--- src/main/java/com/SnsOa/system/util/DataGridPartPageConfig.java
+++
@@ -1,6 +0,0 @@
-package com.SnsOa.system.util;
-
-public class DataGridPartPageConfig {
-	public static final int CRM_LINK_MAN_JS_COUNT_PAGE=45;
-	public static final int CRM_LINK_MAN_JS_CLIENT_COMPANY_COUNT_PAGE=45;
-}
